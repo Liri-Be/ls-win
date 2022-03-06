a76c75481a11550a2beb5489f56be1608bb96ba1
Liri-Be <liri.benzinou@gmail.com>
2022-03-06 18:48:02 +0200

    win-ls

--- ls.bat
+++ ls.bat
@@ -1,4 +1,4 @@
 @echo off
-:: the path to python.exe										the path to my ls.py file				  dir name
+:: the path to python.exe														the path to my ls.py file				  dir name arg
 "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" "C:\Users\liri\PycharmProjects\batch\ls.py" %1
 echo finished
